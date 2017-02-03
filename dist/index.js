5db524c0274d96652323da2bb160c15da4e8462e
Lorenzofox3 <laurent34azerty@gmail.com>
2017-02-03 09:38:00 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global['smart-table-search'] = factory());
+}(this, (function () { 'use strict';
+
+function pointer (path) {
+
+  const parts = path.split('.');
+
+  function partial (obj = {}, parts = []) {
+    const p = parts.shift();
+    const current = obj[p];
+    return (current === undefined || parts.length === 0) ?
+      current : partial(current, parts);
+  }
+
+  function set (target, newTree) {
+    let current = target;
+    const [leaf, ...intermediate] = parts.reverse();
+    for (let key of intermediate.reverse()) {
+      if (current[key] === undefined) {
+        current[key] = {};
+        current = current[key];
+      }
+    }
+    current[leaf] = Object.assign(current[leaf] || {}, newTree);
+    return target;
+  }
+
+  return {
+    get(target){
+      return partial(target, [...parts])
+    },
+    set
+  }
+}
+
+var index = function (searchConf = {}) {
+  const {value, scope = []} = searchConf;
+  const searchPointers = scope.map(field => pointer(field).get);
+  if (!scope.length || !value) {
+    return array => array;
+  } else {
+    return array => array.filter(item => searchPointers.some(p => String(p(item)).includes(String(value))))
+  }
+};
+
+return index;
+
+})));
